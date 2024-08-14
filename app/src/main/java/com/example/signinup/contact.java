2dfaa16d3eacc4b6684844c2c87d45c688efb626
nensiraval <nensiraval@gmail.com>
2024-08-13 18:59:14 -0700

    Thu

--- app/src/main/java/com/example/signinup/contact.java
+++ app/src/main/java/com/example/signinup/contact.java
@@ -1,6 +1,8 @@
 package com.example.signinup;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class contact extends AppCompatActivity {
 
+    Button save;
+    TextInputEditText fname, mael;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        save = findViewById(R.id.save);
+        fname = findViewById(R.id.fname);
+        mael = findViewById(R.id.mael);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
